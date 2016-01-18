83738a306f3bf470edf89cec15b0ed91396c5f45
povergo <jingxuan.han@hotmail.com>
2016-01-18 19:36:17 +0800

    add some new plugins

--- .vim/bundle/get_plugin.sh
+++ .vim/bundle/get_plugin.sh
@@ -4,3 +4,12 @@ git clone https://github.com/Lokaltog/vim-powerline.git
 git clone https://github.com/majutsushi/tagbar.git
 git clone https://github.com/scrooloose/nerdcommenter.git
 git clone https://github.com/scrooloose/nerdtree.git
+git clone git://github.com/godlygeek/tabular.git
+git clone https://github.com/walm/jshint.vim.git
+git clone https://github.com/tomtom/tlib_vim.git
+git clone https://github.com/MarcWeber/vim-addon-mw-utils.git
+git clone https://github.com/garbas/vim-snipmate.git
+git clone https://github.com/scrooloose/syntastic.git
+git clone https://github.com/jamescarr/snipmate-nodejs.git
+git clone https://github.com/guileen/vim-node.git
+git clone https://github.com/myhere/vim-nodejs-complete.git
